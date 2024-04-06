f0bb5825c68c7e1a359625eeda8fdd1a9c68a707
Grisha <grisaelennikov@gmail.com>
2024-04-06 12:10:02 +0300

    Перевел проект на норм тему

---
+++ Pathfinder2E/Pathfinder2E/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Pathfinder2E.ViewModels;
+using Pathfinder2E.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Pathfinder2E
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry
+                .RegisterSingleton<MainWindowViewModel>();
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            //moduleCatalog
+
+        }
+
+        protected override Window CreateShell() => Container.Resolve<MainWindowView>();
+    }
+
+}
---
+++ Pathfinder2E/Pathfinder2E/Converters/VisibilityConvertor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Pathfinder2E.Converters
+{
+    internal class VisibilityConvertor: BooleanConverters<Visibility, VisibilityConvertor>
+    {
+
+        public VisibilityConvertor() : base(Visibility.Visible, Visibility.Collapsed)
+        {
+
+        }
+    }
+}
